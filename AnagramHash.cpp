fcd57d45f7971d49095c04b71783d02a77da8ac6
BrysenA <46940520+BrysenA@users.noreply.github.com>
2020-11-16 10:00:42 -0500

    Add files via upload

---
+++ AnagramHash.cpp
@@ -0,0 +1,33 @@
+/* Brysen Allen, Nick Greiner, Sam Mycroft, Matt Ray
+   Algorthms Project 2: Hashing
+   This is an anagram game. You are given a set of letters and must enter those
+   letters to form words in the word list. The rogram will search using both a 
+   string compare and by comparing hashes. It will then output the time difference
+   between the two search methods.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void anagramSTR(/*entered word*/,/* word list*/)
+{
+  int i;
+  for(i=0;i<len(l);i++)
+    if(strcmp(w,l[i])=0)
+      //send back word
+    else
+      //not in list
+}
+
+void anagramSTR(/*hashed word*/,/* hash list*/)
+{
+  int i;
+  for(i=0;i<len(l);i++)
+    if(hw=h[i])
+      //send back word
+    else
+      //not in list
+}
