9a9140d641bdff7084a6bce275982144263c1ea0
BrysenA <46940520+BrysenA@users.noreply.github.com>
2020-11-19 09:47:51 -0500

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -20,19 +20,53 @@ using namespace std;
 
 vector<int> loadWordfile(string wordfile);
 vector<int> loadHashfile(string hashfile);
+int anagramStr(string entWord,string wordList[]);
+int anagramHash(string entWordHash, string hashList[]);
 
 void runTimeTest();
 
 int main(int argc, char **argv) {
   if (argc == 1) {
-    // ask user for wordfile, then load it, then hash it
+    // ask user for wordfile, then load it, then hash i
+    cout<<"Please enter file name of word list";
+    cin>>inWordFile;
+    loadWordfile();
+    hashalg();
   }
 
   else if (argc == 2) {
     //load provied wordfile, then hash it
+    loadWordfile(/*wordFile*/);
+    hashalg(/*vector holding words*/);
   }
 
   else if (argc == 3) {
     //load provided wordfile and hashfile
+    loadWordfile(/*wordFile*/);
+    loadHashfile(/*hashFile*/);
   }
+  
+  cout<< "Available Letters:";
+}
+//NEED TIME FUNCTION STILL
+int anagramStr(string entWord,string wordList[])
+{
+  int i;
+  for(i=0;i<len(l);i++)
+    if(strcmp(entWord,wordList[i])=0)
+      return 1; //in main add word to found words list
+    else
+      return 0;//in main notify not in list
+}
+
+int anagramHash(string entW
+
+ordHash, string hashList[])
+{
+  int i;
+  for(i=0;i<len(l);i++)
+    if(entWordHash=hashList[i])
+      return 1; //in main add word to found words list
+    else
+      return 0;//in main notify not in list
 }
